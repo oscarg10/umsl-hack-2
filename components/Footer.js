d56d344941670355d3404ae6f24cc6fd5343a525
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-02-22 20:07:12 -0600

    Create JS components4

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default () => `
+<footer>
+  &copy;2020 Equipo
+    <div class="flex justify-center">
+      <span class="fab fa-linkedin"></span>
+      <span class="fab fa-github"></span>
+    </div>
+</footer>
+`;
