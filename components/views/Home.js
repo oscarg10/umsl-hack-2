57acfa115fe7718eeb0266a9f37a8cf85551f7e0
Oscar Gordillo <oscar_andres16@hotmail.com>
2020-02-23 10:40:21 -0600

    Add styles.css and home page

---
+++ components/views/Home.js
@@ -0,0 +1,21 @@
+export default st => `
+<div id="hero" class="flex align-center justify-center" >
+        <a href="./form/" class="button">Contact us</a>
+      </div>
+      <div class="flex--sm">
+        <div>
+          <figure>
+            <img
+              src="https://source.unsplash.com/random/800x600"
+              alt="Random Image"
+            />
+          </figure>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
+            fugiat necessitatibus tenetur placeat? Ex natus sit, incidunt itaque,
+            quam non dolore iusto iste dolorum excepturi voluptatum vel perferendis
+            tempora fugit!
+          </p>
+        </div>
+        </div>
+`;
